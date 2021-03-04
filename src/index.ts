e3a127f085bd1d56d669cfd20e3dc83296687b07
Joel Shin <joel@fluxxlabs.com>
2021-03-04 07:49:05 -0800

    cu

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,6 @@ import { Map } from './Map';
 const user = new User();
 const company = new Company();
 
-console.log(user);
-console.log(company);
-
 const map = new Map('map');
 map.addMarker(user);
 map.addMarker(company);
